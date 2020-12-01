aed47ce949d0de13ff9a6755476e58ff78010764
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2020-11-30 23:56:08 -0500

    updated Index.js with Github user verification

--- index.js
+++ index.js
@@ -4,3 +4,42 @@ const fs = require("fs");
 const template = require('./ReadMe.js');
 const validator = require('email-validator');
 
+let gitHubUserInfo;
+let gitHubRepo;
+let confirmedRepo;
+
+// Confirm existence of Github Account + Repos
+async function verifyGitHubAccount(username) {
+
+    if (username.length === 0)
+        return 'username cannot be blank';
+    else {
+
+        const url = `https://api.github.com/users/${username}`;
+
+        // GitHubUser Data
+        const userResp = await axios.get(url)
+
+        gitHubUserInfo = userResp.data;
+
+        reposUrl = userResp.data.repos_url;
+
+        if (typeof reposUrl === 'undefined')
+            return `GitHub username ${username} does not exist`;
+
+        reposResp = await axios.get(reposUrl);
+
+        const reposData = reposResp.data;
+
+        if (reposData.length === 0)
+            return `GitHub user ${username} has no repos`;
+
+        gitHubRepo = reposData;
+
+        questions[1].choices = reposData.map(repo => repo.name);
+
+        return true;
+    }
+}
+
+
